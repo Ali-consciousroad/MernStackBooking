023ddb1d6a733554e63bb6799f89c93a22aa95a5
Ali-consciousroad <ali_dindar@live.be>
2021-05-10 20:58:25 +0200

    3.10 Express server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
-const name = "Ali";
-module.exports = {
-    name,
-};
+const express = require("express");
+const app = express ();
 
-console.log("====> Node Server", process.version, module.exports);
+// :message is dynamic, it will allow to return whatever was written in the browser
+app.get("/api/:message", (req, res) => {
+    res.status(200).send(`Here is your secret message: ${req.params.message}`);
+});
+
+app.listen(8000, () => console.log(`Server is running on port 8000`));
