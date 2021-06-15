8633bfff5067a1f39f645e00fa7776284e180cb3
Ali-consciousroad <ali_dindar@live.be>
2021-06-15 20:12:04 +0200

    29.32 Process login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,9 +3,10 @@ import express from "express";
 const router = express.Router();
 
 // controllers
-import { register } from "../controllers/auth";
+import { register, login } from "../controllers/auth";
 
 router.post("/register", register);
+router.post("/login", login);
 
 // Common mistake, we can't use this syntax anymore because of the require() in the server.js file
 // Export default router; 
