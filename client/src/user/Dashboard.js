f5fb39c886dd1969dacd78524784653860813655
Ali-consciousroad <ali_dindar@live.be>
2021-07-05 15:52:51 +0200

    6.42 Links on user dashboard page

--- client/src/user/Dashboard.js
+++ client/src/user/Dashboard.js
@@ -1,5 +1,6 @@
 import DashboardNav from "../components/DashboardNav";
 import ConnectNav from "../components/ConnectNav";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
     return (
@@ -12,9 +13,15 @@ const Dashboard = () => {
                 <DashboardNav />
             </div>
 
-            <div className="container">
-                <p>Show all bookings and a button to browser hotels</p>
-                {/*<p>Show all current contracts</p>*/}
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-10">
+                        <h2>Your Bookings</h2>
+                    </div>
+                    <div className="col-md-2">
+                        <Link to="/" className="btn btn-primary">Browse Hotels</Link>
+                    </div>
+                </div>
             </div>
         </>
     );
